7e84be48fb06c8f0c3c274dc675f7f8e3f320424
thomasriedel <49942464+thomasriedel@users.noreply.github.com>
2025-09-22 12:46:20 +0200

    Set base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/triedel",
     plugins: [
         vue(),
         svgLoader({
@@ -20,4 +21,4 @@ export default defineConfig({
     server: {
         port: 8080,
     },
-})
+});
